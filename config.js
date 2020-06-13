1eb8759b3f7c864545adcb58482b2f32907bd170
ambravl <ambra.loos@gmail.com>
2020-06-13 19:48:14 -0300

    added more logging

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = (client) => {
+  console.log(`configDB: ${client.configDB} ||| query: ${client.configDB.query}`);
     let config = client.configDB.query(`SELECT * FROM configDB`).rows;
     console.log(`config has ${config.length} rows`);
     const testing = true;
