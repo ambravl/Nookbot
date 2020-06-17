89121bbf05c1921cb910cc6d17df4236776eca83
ambravl <ambra.loos@gmail.com>
2020-06-17 12:05:52 -0300

    messed up

--- commands/system/toggle.js
+++ commands/system/toggle.js
@@ -1,19 +1,21 @@
-client.exports.run = (client, message, args) => {
-  if (!args[0]) return client.error(message.channel, 'Wrong argument number!', 'You need to provide a command to disable!')
+module.exports.run = (client, message, args) => {
+  if (!args[0]) return client.error(message.channel, 'Wrong argument number!', 'You need to provide a command to disable!');
   const command = client.commands.get(command) || client.commands.get(client.aliases.get(command));
-  if (!command) {
+  if (command) {
+    if (command.help.category === 'system') return client.error(message.channel, 'Forbidden', "You can't disable this command!");
+    else {
+      client.enabledCommands.update(command.help.name, 'false', 'enabled')
+        .then(() => {
+          client.commands.delete(command.help.name);
+        })
+    }
+  } else {
     client.enabledCommands.update(args[0], 'true', 'enabled')
       .then(() => {
         client.commands.set(command.help.name, require(`../../commands/${command.help.category}/${command.help.name}`));
         client.success(message.channel, 'Enabled!', `${args[0]} was successfully enabled!`)
       })
       .catch(() => client.error(message.channel, 'Not found!', `Command ${args[0]} was not found!`));
-  } else if (command.help.category === 'system') return client.error(message.channel, 'Forbidden', "You can't disable this command!");
-  else {
-    client.enabledCommands.update(command.help.name, 'false', 'enabled')
-      .then(() => {
-        client.commands.delete(command.help.name);
-      })
   }
 };
 
