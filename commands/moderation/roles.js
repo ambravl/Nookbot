fdee1f59e928dd054f842fedbc166a326800881e
ambravl <ambra.loos@gmail.com>
2020-06-18 03:47:05 -0300

    changed to matchall

--- commands/moderation/roles.js
+++ commands/moderation/roles.js
@@ -2,36 +2,35 @@ module.exports.run = (client, message, args) => {
   if (!args) return;
   const link = /http.?:..discordapp.com.channels.([0-9]+).([0-9]+).([0-9]+)/.exec(args[0]);
   const roleType = args.length === 2 ? args[1] : 'exclusive';
-  const emojiRE = RegExp("(?:([^\x00-\x7F])|<:\w+:(\d+)>).+?([\w ']+)", 'gu');
+  const emojiRE = /(?:([^\x00-\x7F])|<:\w+:(\d+)>).+?([\w ']+)/gu;
   if (!link) return;
   client.channels.cache.get(link[2]).messages.fetch(link[3])
     .then((msg) => {
-      console.log('fetched message');
-      let emojiArray;
-      client.reactionRoles.insert(link[3], [link[2], roleType], ['channelID', 'type'])
-        .then(() => {
-          console.log('inserted into reaction db');
-          console.log(msg.content);
-          while ((emojiArray = emojiRE.exec(msg.content) !== null)) {
-            console.log(emojiArray);
-            console.log(emojiArray[0]);
-            console.log(emojiArray[1]);
-            console.log(emojiArray[2]);
-            let emojiID;
-            if (emojiArray[0]) emojiID = emojiArray[0];
-            else if (emojiArray[1]) emojiID = message.guild.emojis.cache.find(emoji => emoji.name === emojiArray[1]).identifier;
-            if (emojiID && emojiArray[2]) {
-              const roleID = message.guild.roles.cache.find((r) => r.name.toLowerCase() === emojiArray[2].trim().toLowerCase());
-              msg.react(emojiID)
-                .then(() => {
-                  console.log(`reacted with ${emojiID}`)
-                });
-              client.reactionRoles.push(link[3], {roleID: roleID, emojiID: emojiID}, 'reactions');
-            }
-          }
-        })
+      const matches = msg.content.matchAll(emojiRE);
+      let reactions = [];
+      for (const match of matches) {
+        const emojiID = match[1] ? match[1] : match[2];
+        if (emojiID && match[3]) {
+          const roleID = message.guild.roles.cache.find((r) => r.name.toLowerCase() === match[3].trim().toLowerCase());
+          msg.react(emojiID)
+            .then(() => {
+              console.log(`reacted with ${emojiID}`);
+              reactions.push(`{roleID: "${roleID}", emojiID: "${emojiID}"`);
+            })
+            .catch((err) => {
+              client.handle(err, 'reactionRole setup reaction', message)
+            });
+        }
+      }
+      client.reactionRoles.insert(link[3], [link[2], roleType, `{'${reactions.join("', '")}'}`], ['channelID', 'type', 'reactions'])
+        .catch((err) => {
+          client.handle(err, 'reactionRole setup insertion', message)
+        });
 
     })
+    .catch((err) => {
+      client.handle(err, 'reactionRole setup', message)
+    });
 };
 
 
