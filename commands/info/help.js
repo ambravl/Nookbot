fafd1fa3ff4b8df9ba3d59f78c042422c8c8e41a
ambravl <ambra.loos@gmail.com>
2020-06-19 01:13:56 -0300

    logging

--- commands/info/help.js
+++ commands/info/help.js
@@ -23,6 +23,7 @@ module.exports.run = (client, message, [command], level) => {
   } else {
     let levels = client.levelCache.map((cmd) => cmd.name);
     console.log(levels);
+    console.log(level);
     let commands = client.commands.filter((cmd) => levels.indexOf(cmd.conf.permLevel) <= level
       && client.enabledCommands.get(cmd.help.name) === true);
 
