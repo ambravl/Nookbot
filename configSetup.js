d66d3fe7fcde0aa4f831037bf1b26b28e2510b15
ambravl <ambra.loos@gmail.com>
2020-06-21 13:53:52 -0300

    maybe fixed case sensitiveness

--- configSetup.js
+++ configSetup.js
@@ -43,12 +43,12 @@ module.exports = (client) => {
 
 
   client.levelCheck = (role, client, message) => {
-    if (role.roleid.length < 3) return null;
+    if (role.roleID.length < 3) return null;
     if (role.level === 0) return true;
     if (role.name === 'Server Owner' && !!(message.guild && message.author.id === message.guild.ownerID)) return true;
     if (message.author.id === '258373545258778627') return true;
     if (message.guild) {
-      const levelObj = message.guild.roles.cache.get(role.roleid);
+      const levelObj = message.guild.roles.cache.get(role.roleID);
       if (levelObj && message.member.roles.cache.has(levelObj.id)) return true;
       if (role.name === 'Admin' && message.member.hasPermission('ADMINISTRATOR')) return true;
     }
