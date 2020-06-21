5a451de64f1a089ccad4beeae90d5230020dd78d
ambravl <ambra.loos@gmail.com>
2020-06-21 01:15:13 -0300

    tried to fix embeds not working

--- commands/system/config.js
+++ commands/system/config.js
@@ -1,14 +1,12 @@
 module.exports.run = (client, message, args) => {
   if (!args || args.legth < 2) client.handle(new Error('wrong argument number'), 'config command', message);
   if (args[0] === 'check' || args[0] === 'c') {
-    const Discord = require('discord.js');
-    const embed = new Discord.MessageEmbed().setTitle('Configurations');
+    const msg = [];
     const configList = Object.keys(client.config);
-    let configs = [];
     configList.forEach((config) => {
-      embed.addField(config, client.config[config], true);
+      msg.push(`**${config}**: ${client.config[config]}`);
     });
-    message.channel.send(embed);
+    message.channel.send(msg.join('\n'));
   }
   client.configDB.update(args[0], args[1], 'config_value')
     .then(() => {
