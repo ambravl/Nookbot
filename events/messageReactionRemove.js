38d3f0c55f11889a82b2c63551f689f445c64036
ambravl <ambra.loos@gmail.com>
2020-06-22 00:30:54 -0300

    added logging

--- events/messageReactionRemove.js
+++ events/messageReactionRemove.js
@@ -2,6 +2,7 @@ module.exports = async (client, messageReaction, user) => {
   const reactionRoleMenu = await client.handleReaction(client, messageReaction, user);
   const member = await client.guilds.cache.get(client.config.mainGuild).members.fetch(user.id);
   if (!member || !reactionRoleMenu || !reactionRoleMenu.roleid) return;
+  if (reactionRoleMenu.roleid === client.config.verified) return;
   if (member.roles.cache.has(reactionRoleMenu.roleid)) {
     member.roles.remove(reactionRoleMenu.roleid, '[Auto] Reaction Role Remove')
       .catch((err) => {
