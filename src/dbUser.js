0a758552209f5e9b19dc55e849546d89be1fedce
ambravl <ambra.loos@gmail.com>
2020-06-21 22:56:08 -0300

    made pop actually pop, fixed wrong var name

--- src/dbUser.js
+++ src/dbUser.js
@@ -359,10 +359,10 @@ module.exports = async (client) => {
 SET    ${column} = array_remove(${column}, d.to_remove)
 FROM  (
    SELECT ${this.mainColumn}, to_remove
-   FROM   ${this.name}, unnest(${this.column}) to_remove
+   FROM   ${this.name}, unnest(${column}) to_remove
    WHERE  to_remove->>'${field}' = $2
    ) d
-WHERE  d.${this.mainColumn} = $1`;
+WHERE  d.${this.mainColumn} = $1 RETURNING d.to_remove`;
       // const query = `UPDATE ${this.name} SET ${column} = array_remove(${column}, $1) WHERE ${this.mainColumn} = $2`;
       client.db.query(query, [primaryKey, value])
         .catch((err) => {
