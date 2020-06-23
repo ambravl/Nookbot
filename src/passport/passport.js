cf2dbe5f8bf1107c946c0625b62719c783302bd5
ambravl <ambra.loos@gmail.com>
2020-06-23 13:23:50 -0300

    just playing with canvas

--- src/passport/passport.js
+++ src/passport/passport.js
@@ -1,36 +1,34 @@
 module.exports.Passport = class Passport {
-  async constructor(info) {
+  constructor(info) {
     this.info = info;
-    const Canvas = require('canvas');
-    this.canvas = Canvas.createCanvas(1000, 620);
+    this.Canvas = require('canvas');
+    this.canvas = this.Canvas.createCanvas(1000, 620);
     this.ctx = this.canvas.getContext('2d');
-
-
-    this.placeholderBG = await Canvas.loadImage('./src/passport/placeholder.png');
-    this.bottomBG = await Canvas.loadImage('./src/passport/bottomPattern.png');
-    this.topBG = await Canvas.loadImage('./src/passport/topPattern.png');
-    this.middleBG = await Canvas.loadImage('./src/middlePattern.png');
   }
 
   async placeholder() {
-    this.ctx.drawImage(this.placeholderBG, 0, 0, this.canvas.width, this.canvas.height);
+    const background = await this.Canvas.loadImage('./src/passport/placeholder.png');
+    this.ctx.drawImage(background, 0, 0, this.canvas.width, this.canvas.height);
   }
 
   async bottomBackground() {
     this.ctx.save();
-    this.ctx.globalCompositeOperation = "destination-over";
-    this.ctx.fillStyle = this.ctx.createPattern(this.bottomBG, "repeat-x");
+    this.ctx.globalCompositeOperation = "hue";
+    const background = await this.Canvas.loadImage('./src/passport/bottomPattern.png');
+    this.ctx.fillStyle = this.ctx.createPattern(background, "repeat-x");
     this.ctx.fillRect(0, 850, this.canvas.width, 150);
     this.ctx.restore();
   }
 
   async topBackground() {
-    this.ctx.fillStyle = this.ctx.createPattern(this.topBG, "repeat");
+    const background = await this.Canvas.loadImage('./src/passport/topPattern.png');
+    this.ctx.fillStyle = this.ctx.createPattern(background, "repeat");
     this.ctx.fillRect(0, 0, this.canvas.width, 130);
   }
 
   async middleBackground() {
-    this.ctx.fillStyle = this.ctx.createPattern(this.middleBG, "repeat");
+    const background = await this.Canvas.loadImage('./src/middlePattern.png');
+    this.ctx.fillStyle = this.ctx.createPattern(background, "repeat");
     this.ctx.fillRect(0, 130, this.canvas.width, 720)
   }
 
