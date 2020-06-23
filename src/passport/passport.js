11e3c1e9e37a2dcbbb9efd23d332ac75a27ee481
ambravl <ambra.loos@gmail.com>
2020-06-23 16:53:09 -0300

    just playing with canvas

--- src/passport/passport.js
+++ src/passport/passport.js
@@ -2,51 +2,24 @@ module.exports.Passport = class Passport {
   constructor(info) {
     this.info = info;
     this.Canvas = require('canvas');
-    this.canvas = this.Canvas.createCanvas(1000, 620);
+    this.canvas = this.Canvas.createCanvas(1094, 626);
     this.ctx = this.canvas.getContext('2d');
     this.color = "#AFD528";
   }
 
-  async color() {
-    this.ctx.fillStyle = "#AFD528";
+  async background() {
+    this.ctx.fillStyle = this.color;
     this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-  }
-
-  async bottomBackground() {
     this.ctx.save();
-    this.ctx.globalCompositeOperation = "overlay";
-    const background = await this.Canvas.loadImage('./src/passport/bottomPattern.png');
-    this.ctx.fillStyle = this.ctx.createPattern(background, "repeat-x");
-    this.ctx.fillRect(0, 470, this.canvas.width, 150);
-    this.ctx.restore();
-  }
-
-  async topBackground() {
-    const background = await this.Canvas.loadImage('./src/passport/topPattern.png');
-    this.ctx.fillStyle = this.ctx.createPattern(background, "repeat");
-    this.ctx.fillRect(0, 0, this.canvas.width, 130);
-  }
-
-  async middleBackground() {
-    this.ctx.save();
-    this.ctx.globalAlpha = 0.3;
-    this.ctx.fillStyle = "#ffffff";
-    this.ctx.fillRect(0, 470, this.canvas.width, 150);
-    this.ctx.restore();
-    this.ctx.save();
-    this.ctx.globalCompositeOperation = "darken";
-    this.ctx.globalAlpha = 0.1;
-    const background = await this.Canvas.loadImage('./src/passport/middlePattern.png');
-    this.ctx.fillStyle = this.ctx.createPattern(background, "repeat");
-    this.ctx.fillRect(0, 130, this.canvas.width, 340)
+    this.ctx.globalCompositeOperation = "source-in";
+    const background = await this.Canvas.loadImage('./src/passport/bg.png');
+    this.ctx.fillStyle = this.ctx.createPattern(background, 'no-repeat');
+    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
     this.ctx.restore();
   }
 
   async draw() {
-    await this.color();
-    await this.bottomBackground();
-    await this.topBackground();
-    await this.middleBackground();
+    await this.background();
     return this.canvas.toBuffer();
   }
 }
