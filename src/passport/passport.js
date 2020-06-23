acbc019eef6f4c03989e9400b945a9c7acf09dd1
ambravl <ambra.loos@gmail.com>
2020-06-23 02:33:58 -0300

    changed background to single tile for now

--- src/passport/passport.js
+++ src/passport/passport.js
@@ -8,7 +8,6 @@ module.exports.Passport = class Passport {
 
   async placeholder() {
     const background = await this.Canvas.loadImage('./src/passport/placeholder.png');
-    this.ctx.fillStyle = this.ctx.createPattern(background, "repeat-x");
     this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
   }
 
