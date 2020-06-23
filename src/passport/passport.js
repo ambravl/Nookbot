4ade6f7192739f903ac0360782ad8fde780f8f75
ambravl <ambra.loos@gmail.com>
2020-06-23 15:54:01 -0300

    just playing with canvas

--- src/passport/passport.js
+++ src/passport/passport.js
@@ -4,21 +4,17 @@ module.exports.Passport = class Passport {
     this.Canvas = require('canvas');
     this.canvas = this.Canvas.createCanvas(1000, 620);
     this.ctx = this.canvas.getContext('2d');
+    this.color = "#AFD528";
   }
 
   async color() {
     this.ctx.fillStyle = "#AFD528";
     this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-    this.ctx.save();
-    this.ctx.globalAlpha = 0.7;
-    this.ctx.fillStyle = "#ffffff";
-    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-    this.ctx.restore();
   }
 
   async bottomBackground() {
     this.ctx.save();
-    this.ctx.globalCompositeOperation = "hue";
+    this.ctx.globalCompositeOperation = "overlay";
     const background = await this.Canvas.loadImage('./src/passport/bottomPattern.png');
     this.ctx.fillStyle = this.ctx.createPattern(background, "repeat-x");
     this.ctx.fillRect(0, 470, this.canvas.width, 150);
@@ -32,6 +28,11 @@ module.exports.Passport = class Passport {
   }
 
   async middleBackground() {
+    this.ctx.save();
+    this.ctx.globalAlpha = 0.3;
+    this.ctx.fillStyle = "#ffffff";
+    this.ctx.fillRect(0, 470, this.canvas.width, 150);
+    this.ctx.restore();
     this.ctx.save();
     this.ctx.globalCompositeOperation = "darken";
     this.ctx.globalAlpha = 0.1;
