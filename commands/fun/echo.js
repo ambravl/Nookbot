2e17d1843271b399bf524f3e0416f1552fc45fa4
ambravl <ambra.loos@gmail.com>
2020-06-22 21:50:36 -0300

    had to bruteforce it

--- commands/fun/echo.js
+++ commands/fun/echo.js
@@ -1,19 +1,24 @@
 module.exports.run = (client, message, args, level, Discord) => {
-  const embed = new Discord.MessageEmbed()
-    .setAuthor(message.author.username, message.author.displayAvatarURL());
-  const regexp = /^(?:(title|color|footer|field.+):(.+))|(?:(content|description|text):([\s\S]+))|(?:(url|link): ?(https?:\/\/[^ \n\r]+))/gmu;
-  const matches = message.content.slice(6).matchAll(regexp);
   let channel = message.channel;
-  for (let fields of matches) {
-    if (fields[1]) {
-      const content = fields[2].trim();
-      if (['title', 'color', 'footer'].includes(fields[1])) {
-        embed[`set${fields[1].toProperCase()}`](content);
-      } else if (fields[1].startsWith('field')) embed.addField(fields[1].slice(6), content);
-      else if (fields[1] === 'channel' && level > 3) channel = client.channels.cache.get(content);
-    } else if (fields[5]) embed.setURL(fields[6]);
-    else if (fields[3]) embed.setDescription(fields[4].trim());
-  }
+  if (!isNaN(args[0]) && level > 4) return;
+
+  //   .setAuthor(message.author.username, message.author.displayAvatarURL());
+  //   const regexp = /^(?:(title|color|footer|channel|timestamp|field.+):(.+))|(?:(content|description|text):([\s\S]+))|(?:(url|link): ?(https?:\/\/[^ \n\r]+))|(?:(thumbnail|image|img|icon): ?(https?:\/\/[^ \n\r]+(?:png|gif|jpg|bmp|svg)$))/gmu;
+  // const matches = message.content.slice(6).matchAll(regexp);
+  // let channel = message.channel;
+  // for (let fields of matches) {
+  //   if (fields[1]) {
+  //     const content = fields[2].trim();
+  //     if (['title', 'color', 'footer'].includes(fields[1].toLowerCase())) {
+  //       embed[`set${fields[1].toProperCase()}`](content);
+  //     } else if (fields[1].startsWith('field')) embed.addField(fields[1].slice(6), content);
+  //     else if(fields[1].toLowerCase() === 'timestamp') embed.setTimestamp();
+  //     else if (fields[1].toLowerCase() === 'channel' && level > 3) channel = client.channels.cache.get(content);
+  //   }
+  //   else if (fields[5]) embed.setURL(fields[6]);
+  //
+  //   else if (fields[3]) embed.setDescription(fields[4].trim());
+  // }
   channel.send(embed);
 };
 
@@ -28,6 +33,6 @@ module.exports.help = {
   name: 'echo',
   category: 'fun',
   description: 'Repeats what the user said, but in a fancy embed',
-  usage: 'echo <field>: <text>',
-  details: 'ill write something here someday',
+  usage: 'echo <channel (for mods)> <JSON embed>',
+  details: "Go to https://leovoel.github.io/embed-visualizer/ to visualize the embed you're making , then paste the result here!",
 };
