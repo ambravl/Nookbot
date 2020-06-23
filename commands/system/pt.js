c83cfcb1804db87a5c70b83458e3e163d79de8d9
ambravl <ambra.loos@gmail.com>
2020-06-23 02:42:35 -0300

    made temp command for profile testing outside of a server

---
+++ commands/system/pt.js
@@ -0,0 +1,22 @@
+module.exports.run = async (client, message, args) => {
+  const pass = require('../../src/passport/passport').Passport
+  const passport = new pass({});
+  passport.draw().then((image) => {
+    message.channel.send({files: [new Discord.MessageAttachment(image)]})
+  })
+};
+
+module.exports.conf = {
+  guildOnly: false,
+  aliases: [],
+  permLevel: 'User',
+  allowedChannels: false,
+};
+
+module.exports.help = {
+  name: 'profile',
+  category: 'game',
+  description: 'Profile information display',
+  usage: 'profile <bio|background|islandname|fruit|charactername|hemisphere|profilename|friendcode> <bio|backgrounddurl|name|fruit|hemisphere|code>',
+  details: '<islandname> => Set the name of your \n<fruit> => Set the fruit that is native on your \n<charactername> => Set the name of your character on the \n<hemisphere> => Set the hemisphere your island is in.\n<profilename> => Set the name of your Switch profile.\n<friendcode> => Set your Switch friendcode.',
+};
