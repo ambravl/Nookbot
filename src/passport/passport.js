818e861958962163284f2e9ab81aa4b19b17fcb6
ambravl <ambra.loos@gmail.com>
2020-06-24 00:56:28 -0300

    pixed profile

--- src/passport/passport.js
+++ src/passport/passport.js
@@ -51,8 +51,8 @@ module.exports.Passport = class Passport {
   }
 
   async islandInfo() {
-    const islandIcon = this.Canvas.loadImage('./src/passport/islandName.png');
-    // this.ctx.filter = 'brightness(0.5) sepia(1) saturate(10000%) hue-rotate(120deg)';
+    const islandIcon = await this.Canvas.loadImage('./src/passport/islandName.png');
+    this.ctx.filter = 'brightness(0.5) sepia(1) saturate(10000%) hue-rotate(120deg)';
     this.ctx.drawImage(islandIcon, 424, 212, 40, 40);
     this.ctx.fillStyle = '#59440b';
     this.ctx.font = '24px "Humming"';
