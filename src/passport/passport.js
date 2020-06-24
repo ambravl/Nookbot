ed4b17831bb7ed1ef618a4094daf65b38e483d63
ambravl <ambra.loos@gmail.com>
2020-06-23 21:43:41 -0300

    just playing with canvas

--- src/passport/passport.js
+++ src/passport/passport.js
@@ -84,8 +84,8 @@ module.exports.Passport = class Passport {
     this.ctx.fillStyle = 'rgb(' + pixel[0] + ', ' + pixel[1] + ', ' + pixel[2] + ')';
     this.ctx.moveTo(423, 105);
     this.ctx.lineTo(423 + width, 105);
-    // this.ctx.arc(423 + width, 105, 52, 1.5 * Math.PI, 0.5 * Math.PI);
-    this.ctx.lineTo(423, 157);
+    this.ctx.arc(423 + width, 105, 32, 1.5 * Math.PI, 0.5 * Math.PI);
+    this.ctx.lineTo(423, 167);
     this.ctx.lineTo(423, 105);
     this.ctx.closePath();
     this.ctx.fill();
