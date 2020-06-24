c088a4c662c5d0fde5a0568733a484081a658875
ambravl <ambra.loos@gmail.com>
2020-06-23 21:30:50 -0300

    just playing with canvas

--- src/passport/passport.js
+++ src/passport/passport.js
@@ -27,20 +27,20 @@ module.exports.Passport = class Passport {
     const iconMask = await this.Canvas.loadImage('./src/passport/avvy.png');
     const icon = await this.Canvas.loadImage(this.info.icon);
     ctx.drawImage(iconMask, 0, 0, 245, 245);
-    //ctx.globalCompositeOperation = "source-in";
+    ctx.globalCompositeOperation = "source-in";
     ctx.drawImage(icon, 0, 0, 245, 245);
     return canvas;
   }
 
   async background() {
-    this.ctx.drawImage(await this.drawIcon(), 97, 119);
     const bgMask = await this.Canvas.loadImage('./src/passport/bgMask.png');
     this.ctx.drawImage(bgMask, 0, 0, this.canvas.width, this.canvas.height);
     this.ctx.save();
-    //this.ctx.globalCompositeOperation = "source-out";
+    this.ctx.globalCompositeOperation = "source-out";
     this.ctx.fillStyle = this.color;
     this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
     this.ctx.restore();
+    this.ctx.drawImage(await this.drawIcon(), 97, 119);
     const background = await this.Canvas.loadImage('./src/passport/bg.png');
     this.ctx.drawImage(background, 0, 0, this.canvas.width, this.canvas.height);
   }
